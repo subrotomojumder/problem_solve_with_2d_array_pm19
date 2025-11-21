3a71caa12aeceac56eaee734a8cf85a46d16466f
subrotomojumder <subrotomojumder03@gmail.com>
2025-11-21 10:52:19 +0600

    print digit practice

---
+++ D_Print_Digits_using_Recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void func(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    func(n / 10);
+    int last = n % 10;
+    printf("%d ", last);
+}
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n == 0)
+        {
+            printf("%d", n);
+        }
+        else
+        {
+            func(n);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
